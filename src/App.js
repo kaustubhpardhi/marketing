b17d7b6e5ae71103a018a8f3e29613e768972373
onkar kotule <onkar.kotule@bitcs.in>
2023-02-23 19:32:08 +0530

    my work

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import Home from "./components/home/home";
 import Testimonial from "./components/testimonials/testimonial";
 import Faq from "./components/faq/faq";
 import Tryit from "./components/tryit/tryit";
+import NavBar from "./components/navbar/Navbar";
+import TestFlow from "./components/landing/TestFlow";
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <div className="App">
       <div className="components">
+        <NavBar />
         <Home />
         <Testimonial />
+        <TestFlow />
         <Faq />
         <Tryit />
+        <Footer />
       </div>
     </div>
   );
